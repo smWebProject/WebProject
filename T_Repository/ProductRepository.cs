ed59e2ca02af00f5d3733dd2c0ff34f2e9051b69
Owner <shulamiri@gmail.com>
2022-12-27 02:00:15 +0200

    change and return to source

--- T_Repository/ProductRepository.cs
+++ T_Repository/ProductRepository.cs
@@ -16,20 +16,15 @@ namespace T_Repository
         }
         public async Task<Product[]> GetProducts( string ?name, int? price_from, int? price_to, int[]? categoryIds, int start, int limit, string? direction = "ASC", string? orderBy = "price")
         {
-
             {
-                var query = _context.Products.Where(product => (name == null ? (true) : (product.Name.Contains(name)))
+                  var query = _context.Products.Where(product => (name == null ? (true) : (product.Name.Contains(name)))
                   && ((price_from == null) ? (true) : (product.Price >= price_from))
-                 && (categoryIds.Length == 0) ? (true) : (categoryIds.Contains(product.CategoryId))
+                  && (categoryIds.Length == 0) ? (true) : (categoryIds.Contains(product.CategoryId))
                   && ((price_to == null) ? (true) : (product.Price <= price_to))).OrderBy(product => orderBy);
                 Console.WriteLine(query);
-
                 List<Product> products = query.ToList();
                 return products.ToArray();
-
             }
         }
-
-
         }
 }
