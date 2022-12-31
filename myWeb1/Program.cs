855f1c581a244569451a30c03f1c28cd25e20278
Owner <shulamiri@gmail.com>
2022-12-31 23:44:07 +0200

    add ff

--- myWeb1/Program.cs
+++ myWeb1/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Options;
 using NLog.Web;
 using Service;
 using T_Repository;
-//add
+//add ff
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ShulamitHome");
 builder.Host.UseNLog();
